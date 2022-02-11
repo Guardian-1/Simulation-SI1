5bfa27a1201e4550279b927f97c0478c10459006
Huzalano <charrie26u@etu.univ-lorraine.fr>
2022-02-11 09:23:17 +0100

    correction

--- elevator_simulation/src/EvenementArriveePassagerPalier.java
+++ elevator_simulation/src/EvenementArriveePassagerPalier.java
@@ -33,8 +33,8 @@ public class EvenementArriveePassagerPalier extends Evenement {
 		} else {
 		    assert false : "else impossible";
 		};	
-	    } else {
-			echeancier.ajouter(new EvenementPassageCabinePalier(date + tempsPourBougerLaCabineDUnEtage,p.etageDestination()));
+	    //} else {
+			//echeancier.ajouter(new EvenementPassageCabinePalier(date + tempsPourBougerLaCabineDUnEtage,p.etageDestination()));
 
 		};
 	} else {
