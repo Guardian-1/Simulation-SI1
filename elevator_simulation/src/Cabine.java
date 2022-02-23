0424c6f7e4d75073c640c9c27f5ef2605e9eaca9
David <daorlx541@gmail.com>
2022-02-23 08:46:00 +0100

    test

--- elevator_simulation/src/Cabine.java
+++ elevator_simulation/src/Cabine.java
@@ -115,10 +115,10 @@ public class Cabine extends Global {
 	public void changerEtage (Echeancier echeancier, long date){
 		if(this.intention() == '^'){
 			echeancier.ajouter(new EvenementPassageCabinePalier(date + tempsPourBougerLaCabineDUnEtage, this.etage.getImmeuble().etage(this.etage.numero()+1)));
-
 		}else if(this.intention() == 'v'){
 			echeancier.ajouter(new EvenementPassageCabinePalier(date + tempsPourBougerLaCabineDUnEtage, this.etage.getImmeuble().etage(this.etage.numero()-1)));
-
+		} else if (this.intention() == '-') {
+			echeancier.ajouter(new EvenementOuverturePorteCabine(date+tempsPourOuvrirOuFermerLesPortes));
 		}
 	}
 }
--- elevator_simulation/src/EvenementArriveePassagerPalier.java
+++ elevator_simulation/src/EvenementArriveePassagerPalier.java
@@ -1,53 +1,50 @@
 public class EvenementArriveePassagerPalier extends Evenement {
-	/* APP: Arrivée Passager Palier
+    /* APP: Arrivée Passager Palier
        L'instant précis ou un nouveau passager arrive sur un palier donné, dans le but
        de monter dans la cabine.
-	 */
+    */
+    
+    private Etage etage;
 
-	private Etage etage;
+    public EvenementArriveePassagerPalier(long d, Etage edd) {
+	super(d);
+	etage = edd;
+    }
 
-	public EvenementArriveePassagerPalier(long d, Etage edd) {
-		super(d);
-		etage = edd;
+    public void afficheDetails(StringBuilder buffer, Immeuble immeuble) {
+	buffer.append("APP ");
+	buffer.append(etage.numero());
+    }
+
+    public void traiter(Immeuble immeuble, Echeancier echeancier) {
+	assert etage != null;
+	assert immeuble.etage(etage.numero()) == etage;
+	Passager p = new Passager(date, etage, immeuble);
+	Cabine c = immeuble.cabine;
+
+
+	if (c.porteOuverte && c.etage == etage) {
+	    if (c.intention() == '-') {
+		c.changerIntention(p.sens());
+		echeancier.ajouter(new EvenementFermeturePorteCabine(date + tempsPourOuvrirOuFermerLesPortes)); 
+		char fmp = c.faireMonterPassager(p);
+		if (fmp == 'O') {
+		    assert true;
+		} else {
+		    assert false : "else impossible";
+		};	
+	    //} else {
+			//echeancier.ajouter(new EvenementPassageCabinePalier(date + tempsPourBougerLaCabineDUnEtage,p.etageDestination()));
+
+		};
+	} else {
+		//if (c.porteOuverte){
+		//	echeancier.ajouter(new EvenementOuverturePorteCabine(date+tempsPourOuvrirOuFermerLesPortes));
+		//}
 	}
+	date+=etage.arriveeSuivante();
+	echeancier.ajouter(this);
+	assert c.intention() != '-' : "intention impossible";
+    }
 
-	public void afficheDetails(StringBuilder buffer, Immeuble immeuble) {
-		buffer.append("APP ");
-		buffer.append(etage.numero());
-	}
-
-	public void traiter(Immeuble immeuble, Echeancier echeancier) {
-		assert etage != null;
-		assert immeuble.etage(etage.numero()) == etage;
-		Passager p = new Passager(date, etage, immeuble);
-		Cabine c = immeuble.cabine;
-
-
-		if (c.porteOuverte && c.etage == etage) {
-			switch (c.intention()) {
-			case '-': 
-				c.changerIntention(p.sens());
-				echeancier.ajouter(new EvenementFermeturePorteCabine(date + tempsPourOuvrirOuFermerLesPortes)); 
-				char fmp = c.faireMonterPassager(p);
-				if (fmp == 'O') {
-					assert true;
-				} else {
-					assert false : "else impossible";
-				};	
-				break;
-			case '^':
-				c.changerEtage(echeancier, date);
-				break;
-
-			case 'v':
-				c.changerEtage(echeancier, date);
-				break;	
-			}
-
-			date+=etage.arriveeSuivante();
-			echeancier.ajouter(this);
-			assert c.intention() != '-' : "intention impossible";
-		}
-
-	}
 }
