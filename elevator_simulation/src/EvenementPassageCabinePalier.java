b397d8ff6cfffd0d7d1a94ee8202b6e194c7d32a
David <daorlx541@gmail.com>
2022-03-03 08:13:23 +0100

    TEST

--- elevator_simulation/src/EvenementPassageCabinePalier.java
+++ elevator_simulation/src/EvenementPassageCabinePalier.java
@@ -1,31 +1,30 @@
 public class EvenementPassageCabinePalier extends Evenement {
-    /* PCP: Passage Cabine Palier
+	/* PCP: Passage Cabine Palier
        L'instant precis où la cabine passe juste en face d'un etage precis.
        Vous pouvez modifier cette classe comme vous voulez (ajouter/modifier des methodes etc.).
-    */
-    
-    private Etage etage;
-    
-    public EvenementPassageCabinePalier(long d, Etage e) {
-	super(d);
-	etage = e;
-    }
-    
-    public void afficheDetails(StringBuilder buffer, Immeuble immeuble) {
-	buffer.append("PCP ");
-	buffer.append(etage.numero());
-    }
-    
-    public void traiter(Immeuble immeuble, Echeancier echeancier) {
-	Cabine cabine = immeuble.cabine;
-	assert ! cabine.porteOuverte;
-	assert etage.numero() != cabine.etage.numero();
+	 */
+
+	private Etage etage;
+
+	public EvenementPassageCabinePalier(long d, Etage e) {
+		super(d);
+		etage = e;
+	}
+
+	public void afficheDetails(StringBuilder buffer, Immeuble immeuble) {
+		buffer.append("PCP ");
+		buffer.append(etage.numero());
+	}
+
+	public void traiter(Immeuble immeuble, Echeancier echeancier) {
+		Cabine cabine = immeuble.cabine;
+		assert ! cabine.porteOuverte;
+		assert etage.numero() != cabine.etage.numero();
 		cabine.etage = etage;
 		if (cabine.doitSarreter(echeancier,date)){
-		echeancier.ajouter(new EvenementOuverturePorteCabine(date + tempsPourOuvrirOuFermerLesPortes));
-	}else{
-		System.out.println("TEST");
-		cabine.changerEtage(echeancier,date);
+			echeancier.ajouter(new EvenementOuverturePorteCabine(date + tempsPourOuvrirOuFermerLesPortes));
+		}else{
+			cabine.changerEtage(echeancier,date);
+		}
 	}
-    }
 }
