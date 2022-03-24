424189ec59d2639816ed0fd829ad59fbf91bf10f
David <daorlx541@gmail.com>
2022-03-24 08:44:16 +0100

    test

--- elevator_simulation/src/EvenementOuverturePorteCabine.java
+++ elevator_simulation/src/EvenementOuverturePorteCabine.java
@@ -21,7 +21,7 @@ public class EvenementOuverturePorteCabine extends Evenement {
 		cabine.ouvrirPorte();
 		cabine.recalculerIntention(echeancier,immeuble,date);
 		c = c + etage.faireEntrerPassager(cabine,echeancier);
-		if (cabine.intention() != '-') {
+		if (cabine.porteOuverte && cabine.intention() != '-') {
 		    echeancier.ajouter(new EvenementFermeturePorteCabine(date + tempsPourOuvrirOuFermerLesPortes + (c * tempsPourEntrerOuSortirDeLaCabine)));
                 };
 		assert cabine.porteOuverte;
