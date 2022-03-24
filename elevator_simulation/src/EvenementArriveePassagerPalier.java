72ed7fe975324a4e7512de8686b8116fc690b884
David <daorlx541@gmail.com>
2022-03-25 00:18:34 +0100

    test

--- elevator_simulation/src/EvenementArriveePassagerPalier.java
+++ elevator_simulation/src/EvenementArriveePassagerPalier.java
@@ -1,65 +1,66 @@
 public class EvenementArriveePassagerPalier extends Evenement {
-    /* APP: Arrivée Passager Palier
+	/* APP: Arrivée Passager Palier
        L'instant précis ou un nouveau passager arrive sur un palier donné, dans le but
        de monter dans la cabine.
-    */
-    
-    private Etage etage;
+	 */
 
-    public EvenementArriveePassagerPalier(long d, Etage edd) {
-	super(d);
-	etage = edd;
-    }
+	private Etage etage;
 
-    public void afficheDetails(StringBuilder buffer, Immeuble immeuble) {
-	buffer.append("APP ");
-	buffer.append(etage.numero());
-    }
+	public EvenementArriveePassagerPalier(long d, Etage edd) {
+		super(d);
+		etage = edd;
+	}
 
-    public void traiter(Immeuble immeuble, Echeancier echeancier) {
-    assert etage != null;
-    
-    assert immeuble.etage(etage.numero()) == etage;
-    Passager p = new Passager(date, etage, immeuble);
-    Cabine c = immeuble.cabine;
-    
+	public void afficheDetails(StringBuilder buffer, Immeuble immeuble) {
+		buffer.append("APP ");
+		buffer.append(etage.numero());
+	}
 
-    
-    if (c.porteOuverte && c.etage == etage) {
-        if (c.intention() == '-') {
-        c.changerIntention(p.sens());
-        if(c.porteOuverte) {
-            echeancier.ajouter(new EvenementFermeturePorteCabine(date + tempsPourOuvrirOuFermerLesPortes));
-        }        
-        char fmp = c.faireMonterPassager(p);
-        if (fmp == 'O') {
-            assert true;
-        } else {
-            assert false : "else impossible";
-        };    
-        } else {
-            //echeancier.ajouter(new EvenementPassageCabinePalier(date + tempsPourBougerLaCabineDUnEtage,p.etageDestination()));
+	public void traiter(Immeuble immeuble, Echeancier echeancier) {
+		assert etage != null;
 
-        };
-    } else {
-        this.etage.ajouter(p);
-        if (c.etage != etage)
-            if (c.intention()=='-') {
-                if (etage.numero() > c.etage.numero())
-                    c.changerIntention('^');
-                else c.changerIntention('v');
-            }
-            if (immeuble.passagerAuDessus(etage) || !c.isEmpty() || immeuble.passagerEnDessous(etage) && c.porteOuverte)
-            echeancier.ajouter(new EvenementFermeturePorteCabine(date+tempsPourOuvrirOuFermerLesPortes));
-            echeancier.ajouter(new EvenementPietonArrivePalier(date+delaiDePatienceAvantSportif,etage,p));
+		assert immeuble.etage(etage.numero()) == etage;
+		Passager p = new Passager(date, etage, immeuble);
+		Cabine c = immeuble.cabine;
 
-        //char fff = c.faireMonterPassager(p);
 
-    }
 
-    date+=etage.arriveeSuivante();
-    echeancier.ajouter(this);
-    assert c.intention() != '-' : "intention impossible";
-    }
+		if (c.porteOuverte && c.etage == etage) {
+			if (c.intention() == '-') {
+				c.changerIntention(p.sens());
+				if(c.porteOuverte) {
+					echeancier.ajouter(new EvenementFermeturePorteCabine(date + tempsPourOuvrirOuFermerLesPortes));
+				}        
+				char fmp = c.faireMonterPassager(p);
+				if (fmp == 'O') {
+					assert true;
+				} else {
+					assert false : "else impossible";
+				};    
+			} else {
+				//echeancier.ajouter(new EvenementPassageCabinePalier(date + tempsPourBougerLaCabineDUnEtage,p.etageDestination()));
+
+			};
+		} else {
+			this.etage.ajouter(p);
+			if (c.etage != etage) {
+				if (c.intention()=='-') {
+					if (etage.numero() > c.etage.numero())
+						c.changerIntention('^');
+					else c.changerIntention('v');
+				}
+			}
+			if (c.porteOuverte)
+				echeancier.ajouter(new EvenementFermeturePorteCabine(date+tempsPourOuvrirOuFermerLesPortes));
+			echeancier.ajouter(new EvenementPietonArrivePalier(date+delaiDePatienceAvantSportif,etage,p));
+
+			//char fff = c.faireMonterPassager(p);
+
+		}
+
+		date+=etage.arriveeSuivante();
+		echeancier.ajouter(this);
+		assert c.intention() != '-' : "intention impossible";
+	}
 
 }
