4a2f8e4eed4544998e0012dce795986bd7b7aa5c
David <daorlx541@gmail.com>
2022-03-24 08:55:14 +0100

    rollback

--- elevator_simulation/src/EvenementOuverturePorteCabine.java
+++ elevator_simulation/src/EvenementOuverturePorteCabine.java
@@ -1,17 +1,17 @@
 public class EvenementOuverturePorteCabine extends Evenement {
-    /* OPC: Ouverture Porte Cabine
+	/* OPC: Ouverture Porte Cabine
        L'instant précis ou la porte termine de s'ouvrir.
-    */
-    
-    public EvenementOuverturePorteCabine(long d) {
-	super(d);
-    }
-    
-    public void afficheDetails(StringBuilder buffer, Immeuble immeuble) {
-	buffer.append("OPC");
-    }
-    
-    public void traiter(Immeuble immeuble, Echeancier echeancier) {
+	 */
+
+	public EvenementOuverturePorteCabine(long d) {
+		super(d);
+	}
+
+	public void afficheDetails(StringBuilder buffer, Immeuble immeuble) {
+		buffer.append("OPC");
+	}
+
+	public void traiter(Immeuble immeuble, Echeancier echeancier) {
 		Cabine cabine = immeuble.cabine;
 		Etage etage = cabine.etage;
 		int c = 0;
@@ -21,11 +21,11 @@ public class EvenementOuverturePorteCabine extends Evenement {
 		cabine.ouvrirPorte();
 		cabine.recalculerIntention(echeancier,immeuble,date);
 		c = c + etage.faireEntrerPassager(cabine,echeancier);
-//		if (cabine.porteOuverte && cabine.intention() != '-') {
-//		    echeancier.ajouter(new EvenementFermeturePorteCabine(date + tempsPourOuvrirOuFermerLesPortes + (c * tempsPourEntrerOuSortirDeLaCabine)));
-//                };
+		if (cabine.porteOuverte && cabine.intention() != '-') {
+			echeancier.ajouter(new EvenementFermeturePorteCabine(date + tempsPourOuvrirOuFermerLesPortes + (c * tempsPourEntrerOuSortirDeLaCabine)));
+		};
 		assert cabine.porteOuverte;
 
-	
+
 	}
 }
